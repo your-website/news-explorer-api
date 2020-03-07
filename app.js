ba0f0bd10dc6d25b161bb10216f58e2767647f3c
Roman Kolpachev <roman.kolpachev@yandex.ru>
2020-03-07 16:24:11 +0700

    add CORS Auth

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(function(req, res, next) {
         res.header('Access-Control-Allow-Origin', origin);
     }
 
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
 
     next();
