577129ec4f125881522117c633cdd368fcb7a90d
Roman Kolpachev <roman.kolpachev@yandex.ru>
2020-03-07 15:47:50 +0700

    CORS

--- app.js
+++ app.js
@@ -27,6 +27,9 @@ app.use(function(req, res, next) {
         res.header('Access-Control-Allow-Origin', origin);
     }
 
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+
     next();
 });
 app.use(limiter);
