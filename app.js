150c8fdb8ba480adaf30cd54a06f67b212eda7a1
Roman Kolpachev <roman.kolpachev@yandex.ru>
2020-03-08 11:29:53 +0700

    CORS headers

--- app.js
+++ app.js
@@ -21,13 +21,8 @@ const allowedCors = [
 ];
 
 app.use(function(req, res, next) {
-    const { origin } = req.headers; // Записываем в переменную origin соответствующий заголовок
-
-    if (allowedCors.includes(origin)) { // Проверяем, что значение origin есть среди разрешённых доменов
-        res.header('Access-Control-Allow-Origin', origin);
-    }
-
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, authorization');
+    res.header("Access-Control-Allow-Origin", '*')
+    res.header('Access-Control-Allow-Headers', '*');
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
 
     next();
