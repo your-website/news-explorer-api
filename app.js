b0852cda0b58b37e9832bc884b9e54f427028681
Roman Kolpachev <roman.kolpachev@yandex.ru>
2020-03-08 11:20:03 +0700

    CORS headers

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ const allowedCors = [
 
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Headers', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
 
     next();
